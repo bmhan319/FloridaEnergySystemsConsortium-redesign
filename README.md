a988987f7928ca707dc0c6be3a15ebcd913bc227
Brian Han <bmhan319@hotmail.com>
2018-08-07 15:27:55 -0700

    running charts with api data

--- README.md
+++ README.md
@@ -16,9 +16,11 @@ Charts
 Added a couple charts on the "NavBar--> About US--> Energy Reports" page using Google Charts.
 
 Chart 1 - Line Chart - https://developers.google.com/chart/interactive/docs/gallery/linechart
+
 Options Used: title, curveType, legend, colors
 
 Chart 2 - Scatter Chart - https://developers.google.com/chart/interactive/docs/gallery/scatterchart
+
 Options Used: title, hAxis, vAxis, legend, colors
 
 Due to the types of values that were being plotted, only a handful of charts made sense to use. With only "Year" and "BTUs" as plot points, line graphs, scatter graphs and bar graphs were the best options. And with having 48 years to plot, the bar graph would've looked overcrowded so the remaining two options were used.
---
+++ scripts/reports-charts2.js
@@ -0,0 +1,100 @@
+// 1 DomContentLoad
+// 2 Google Cahrt
+// 3 Get API DAta
+// 4 Receive API DAta
+// 5 Draw Chart
+//ffdbc2ac0fd91596a58b9cf3fb6c29bd
+
+
+function onDOMLoad() {
+  google.charts.load('current', {'packages':['corechart']});
+  google.charts.setOnLoadCallback(getData);
+  google.charts.setOnLoadCallback(getData2);
+}
+
+document.addEventListener("DOMContentLoaded", onDOMLoad);
+
+function getData() {
+  let request = new XMLHttpRequest();
+  let requestUrl = "http://api.eia.gov/series/?api_key=ffdbc2ac0fd91596a58b9cf3fb6c29bd&series_id=SEDS.REPRB.FL.A"
+  request.open('GET', requestUrl, true)
+  request.onload = function(){
+    console.log("response from server is: ", request)
+    if (request.status !== 200) {
+      console.log("Something is wrong", request);
+      return;
+    }
+    let response = JSON.parse(request.response);
+    drawChart(response.series[0].data);
+  }
+  
+  request.error = function(err){
+    console.log("error is: ", err);
+  }
+  request.send();
+}
+
+function getData2() {
+  let request = new XMLHttpRequest();
+  let requestUrl = "http://api.eia.gov/series/?api_key=ffdbc2ac0fd91596a58b9cf3fb6c29bd&series_id=SEDS.TETCB.FL.A"
+  request.open('GET', requestUrl, true)
+  request.onload = function(){
+    console.log("response from server is: ", request)
+    if (request.status !== 200) {
+      console.log("Something is wrong", request);
+      return;
+    }
+    let response = JSON.parse(request.response);
+    drawChart2(response.series[0].data);
+  }
+  
+  request.error = function(err){
+    console.log("error is: ", err);
+  }
+  request.send();
+}
+
+function drawChart(freshData) {
+  let headerArray = ['Year' , 'BTUs (in billions)'];
+  let flippedData = freshData;
+  let fixedData = flippedData.reverse();
+  fixedData.unshift(headerArray);
+  let data = google.visualization.arrayToDataTable(fixedData);
+
+  let options = {
+    title: 'Florida Energy Production',
+    hAxis: {title: 'Year'},
+    vAxis: {title: 'BTUs (in billions)'},
+    colors: ['#618b44'],
+    legend: 'none',
+  };
+
+  let chart = new google.visualization.ScatterChart(document.getElementById('curve_chart'));
+
+  chart.draw(data, options);
+}
+
+
+function drawChart2(freshData) {
+  let headerArray = ['Year' , 'BTUs (in billions)'];
+  let flippedData = freshData;
+  let fixedData = flippedData.reverse();
+  fixedData.unshift(headerArray);
+  let data = google.visualization.arrayToDataTable(fixedData)
+
+  let options = {
+    title: 'Florida Electrictiy Consumption',
+    curveType: 'function',
+    legend: { position: 'bottom' },
+    colors: ['#FF8811']
+  };
+
+  let chart = new google.visualization.LineChart(document.getElementById('curve_chart2'));
+
+  chart.draw(data, options);
+}
+
+$(window).resize(function(){
+   drawChart();
+  drawChart2();
+ });
